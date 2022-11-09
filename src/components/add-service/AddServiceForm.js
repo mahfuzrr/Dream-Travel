30b6f49fa35e60e61560815b9ce73603d80bbebc
Mahfuzur Rahman <51076032+Mahfuzz11@users.noreply.github.com>
2022-11-09 22:54:16 +0600

    updating add service

--- src/components/add-service/AddServiceForm.js
+++ src/components/add-service/AddServiceForm.js
@@ -1,30 +1,87 @@
+import { useState } from 'react';
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 /* eslint-disable jsx-a11y/label-has-associated-control */
 export default function AddServiceForm() {
+    const [title, setTitle] = useState('');
+    const [price, setPrice] = useState('');
+    const [photoURL, setPhotoURL] = useState('');
+    const [description, setDescription] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        const obj = {
+            title,
+            price,
+            photoURL,
+            description,
+        };
+
+        fetch('http://localhost:5000/add-services', { method: 'POST', body: obj })
+            .then(() => {
+                toast.success('Service Added Successfully!', {
+                    position: toast.POSITION.TOP_RIGHT,
+                });
+            })
+            .catch((err) => {
+                toast.error(err.message, {
+                    position: toast.POSITION.TOP_RIGHT,
+                });
+            });
+    };
+
     return (
         <div className="container-fluid overflow-hidden min-vh-100" id="add-service-content">
+            <ToastContainer />
             <div className="container" id="add-service-all">
                 <h4 className="text-center">Add a service</h4>
-                <form action="#" id="add-service-form">
+                <form onSubmit={handleSubmit} id="add-service-form">
                     <div className="form-group">
-                        <input type="text" className="form-control" name="title" required />
+                        <input
+                            type="text"
+                            className="form-control"
+                            name="title"
+                            onChange={(e) => setTitle(e.target.value)}
+                            required
+                        />
                         <label className="form-label" htmlFor="title">
                             Title
                         </label>
                     </div>
                     <div className="form-group">
-                        <input type="text" className="form-control" name="price" required />
+                        <input
+                            type="text"
+                            className="form-control"
+                            name="price"
+                            onChange={(e) => setPrice(e.target.value)}
+                            required
+                        />
                         <label className="form-label" htmlFor="price">
                             Price
                         </label>
                     </div>
                     <div className="form-group">
-                        <input type="text" className="form-control" name="url" required />
+                        <input
+                            type="text"
+                            className="form-control"
+                            name="url"
+                            onChange={(e) => setPhotoURL(e.target.value)}
+                            required
+                        />
                         <label className="form-label" htmlFor="url">
                             Photo Url
                         </label>
                     </div>
                     <div className="form-group">
-                        <textarea className="form-control" id="descr" rows="3" />
+                        <textarea
+                            className="form-control"
+                            id="descr"
+                            rows="3"
+                            onChange={(e) => setDescription(e.target.value)}
+                            required
+                        />
                         <label className="form-label" htmlFor="descr">
                             Description
                         </label>
